bcbc2dca3f3e1bbd6162f942ebd95f973bf70319
WarmagLit <genzeartem0@gmail.com>
2021-12-20 10:40:12 +0700

    Full screen

--- app/src/main/java/com/tsu/mobilegamelab4/Menu/MenuActivity.kt
+++ app/src/main/java/com/tsu/mobilegamelab4/Menu/MenuActivity.kt
@@ -1,9 +1,10 @@
-package com.tsu.mobilegamelab4
+package com.tsu.mobilegamelab4.Menu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tsu.mobilegamelab4.R
 
-class MainActivity : AppCompatActivity() {
+class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
