d419b28118d34cdefb961148bdcd223b5dc13b61
WarmagLit <genzeartem0@gmail.com>
2022-01-22 13:52:39 +0700

    Collisions

--- app/src/main/java/com/tsu/mobilegamelab4/game/map/bonuslocation/BonusLocationCollisionLayout.kt
+++ app/src/main/java/com/tsu/mobilegamelab4/game/map/bonuslocation/BonusLocationCollisionLayout.kt
@@ -4,5 +4,5 @@ import com.tsu.mobilegamelab4.game.map.CollisionLayout
 
 class BonusLocationCollisionLayout: CollisionLayout() {
     var layin = Array(BonusLocationMap.NUMBER_OF_ROW_CELLS) {Array(BonusLocationMap.NUMBER_OF_COLUMN_CELLS){0} }
-    
+
 }
